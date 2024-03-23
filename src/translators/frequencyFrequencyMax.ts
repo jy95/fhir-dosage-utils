4070f9fd475670d2ce972c8eebebec89171c4ef5
jy95 <jacques.yakoub@gmail.com>
2024-03-23 11:45:46 +0100

    feat: frequency / frequencyMax translation

---
+++ src/translators/frequencyFrequencyMax.ts
@@ -0,0 +1,39 @@
+import i18next from "i18next";
+import type { Dosage } from "fhir/r4";
+
+export function transformDurationDurationMaxToText(
+  dos: Dosage,
+): string | undefined {
+  // If empty, return undefined
+  if (dos.timing == undefined || dos.timing.repeat == undefined) {
+    return undefined;
+  }
+
+  // Pickup the repeat interesting attributes
+  let repeat = dos.timing.repeat;
+  let frequency = repeat.frequency;
+  let max = repeat.frequencyMax;
+
+  // Do nothing if no frequency / frequencyMax, I am not a wizard
+  if (frequency === undefined && max === undefined) {
+    return undefined;
+  } else {
+    // Three cases
+
+    // 1. Frequency and frequencyMax are present
+    if (frequency !== undefined && max !== undefined) {
+      return i18next.t("fields.frequency.withfrequencyMax", {
+        count: max,
+        frequency: frequency,
+      });
+    }
+
+    // 2. Only frequencyMax is present
+    if (max !== undefined) {
+      return i18next.t("fields.frequencyMax.frequencyMax", { count: max });
+    }
+
+    // 3. Only frequency is present
+    return i18next.t("fields.frequency.onlyFrequency", { count: frequency });
+  }
+}
