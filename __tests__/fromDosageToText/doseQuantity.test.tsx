4bde3022c74cd9dc88d5596c5320e13a1ef281fe
jy95 <jacques.yakoub@gmail.com>
2024-03-24 19:36:32 +0100

    test: doseQuantity

---
+++ __tests__/fromDosageToText/doseQuantity.test.tsx
@@ -0,0 +1,50 @@
+// For typings autocomplete whatever your IDE
+import { expect, test, describe } from "@jest/globals";
+import FhirDosageUtils from "../../src/index";
+
+// types
+import type { Dosage } from "fhir/r4";
+
+describe("fromDosageToText - doseQuantity", () => {
+  let dosageUtils: FhirDosageUtils;
+
+  beforeAll(async () => {
+    dosageUtils = await FhirDosageUtils.build({
+      displayOrder: ["doseQuantity"],
+    });
+  });
+
+  test("No doseQuantity", () => {
+    const dosage: Dosage = {
+      text: "no doseQuantity",
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("Empty", () => {
+    const dosage: Dosage = {
+      doseAndRate: [],
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("Simple case", () => {
+    const dosage: Dosage = {
+      doseAndRate: [
+        {
+          doseQuantity: {
+            value: 3,
+            unit: "pills",
+          },
+        },
+      ],
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("3 pills");
+  });
+});
