64e96c2bba93596c009632bf19f4b3bc7b6dc124
jy95 <jacques.yakoub@gmail.com>
2024-03-24 19:26:25 +0100

    test: dayOfWeek

---
+++ __tests__/fromDosageToText/dayOfWeek.test.tsx
@@ -0,0 +1,64 @@
+// For typings autocomplete whatever your IDE
+import { expect, test, describe } from "@jest/globals";
+import FhirDosageUtils from "../../src/index";
+
+// types
+import type { Dosage } from "fhir/r4";
+
+describe("fromDosageToText - dayOfWeek", () => {
+  let dosageUtils: FhirDosageUtils;
+
+  beforeAll(async () => {
+    dosageUtils = await FhirDosageUtils.build({
+      displayOrder: ["dayOfWeek"],
+    });
+  });
+
+  test("No dayOfWeek", () => {
+    const dosage: Dosage = {
+      text: "no dayOfWeek",
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("Empty", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          dayOfWeek: [],
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("1 item", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          dayOfWeek: ["fri"],
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("on Friday");
+  });
+
+  test("N+1 items", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          dayOfWeek: ["mon", "fri"],
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("on Monday and Friday");
+  });
+});
