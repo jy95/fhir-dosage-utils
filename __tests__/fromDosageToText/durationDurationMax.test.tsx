5f0770c893b381618187ce2f1729f8ec7c6ce766
jy95 <jacques.yakoub@gmail.com>
2024-03-24 20:36:05 +0100

    test: durationDurationMax

---
+++ __tests__/fromDosageToText/durationDurationMax.test.tsx
@@ -0,0 +1,68 @@
+// For typings autocomplete whatever your IDE
+import { expect, test, describe } from "@jest/globals";
+import FhirDosageUtils from "../../src/index";
+
+// types
+import type { Dosage } from "fhir/r4";
+
+describe("fromDosageToText - durationDurationMax", () => {
+  let dosageUtils: FhirDosageUtils;
+
+  beforeAll(async () => {
+    dosageUtils = await FhirDosageUtils.build({
+      displayOrder: ["durationDurationMax"],
+    });
+  });
+
+  test("No countCountMax", () => {
+    const dosage: Dosage = {
+      text: "no durationDurationMax",
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("duration and DurationMax", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          duration: 1,
+          durationMax: 3,
+          durationUnit: "d",
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("over 1 day (maximum 3 days)");
+  });
+
+  test("durationMax only", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          durationMax: 5,
+          durationUnit: "d",
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("(maximum 5 days)");
+  });
+
+  test("duration only", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          duration: 3,
+          durationUnit: "d",
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("over 3 days");
+  });
+});
