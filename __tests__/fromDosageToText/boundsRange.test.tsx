54db483fbaecdd7f313ff7e4119ceb9ff8df0316
jy95 <jacques.yakoub@gmail.com>
2024-03-24 18:27:33 +0100

    test: boundsRange

---
+++ __tests__/fromDosageToText/boundsRange.test.tsx
@@ -0,0 +1,88 @@
+// For typings autocomplete whatever your IDE
+import { expect, test, describe } from "@jest/globals";
+import FhirDosageUtils from "../../src/index";
+
+// types
+import type { Dosage } from "fhir/r4";
+
+describe("fromDosageToText - boundsRange", () => {
+  let dosageUtils: FhirDosageUtils;
+
+  beforeAll(async () => {
+    dosageUtils = await FhirDosageUtils.build({
+      displayOrder: ["boundsRange"],
+    });
+  });
+
+  test("No boundsRange", () => {
+    const dosage: Dosage = {
+      text: "no boundsRange",
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("high and low", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          boundsRange: {
+            low: {
+              system: "http://hl7.org/fhir/ValueSet/duration-units",
+              value: 3,
+              code: "d",
+            },
+            high: {
+              system: "http://hl7.org/fhir/ValueSet/duration-units",
+              value: 5,
+              code: "d",
+            },
+          },
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("for 3 to 5 days");
+  });
+
+  test("high only", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          boundsRange: {
+            high: {
+              system: "http://hl7.org/fhir/ValueSet/duration-units",
+              value: 5,
+              code: "d",
+            },
+          },
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("for up to 5 days");
+  });
+
+  test("low only", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          boundsRange: {
+            low: {
+              system: "http://snomed.info/sct",
+              value: 5,
+              code: "258703001",
+              unit: "days",
+            },
+          },
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("for at least 5 days");
+  });
+});
