3d5e6db0afe83d9d534be1efd7829846baf5300b
jy95 <jacques.yakoub@gmail.com>
2024-03-25 19:03:49 +0100

    test: periodPeriodMax

---
+++ __tests__/fromDosageToText/periodPeriodMax.test.tsx
@@ -0,0 +1,68 @@
+// For typings autocomplete whatever your IDE
+import { expect, test, describe } from "@jest/globals";
+import FhirDosageUtils from "../../src/index";
+
+// types
+import type { Dosage } from "fhir/r4";
+
+describe("fromDosageToText - periodPeriodMax", () => {
+  let dosageUtils: FhirDosageUtils;
+
+  beforeAll(async () => {
+    dosageUtils = await FhirDosageUtils.build({
+      displayOrder: ["periodPeriodMax"],
+    });
+  });
+
+  test("No periodPeriodMax", () => {
+    const dosage: Dosage = {
+      text: "no periodPeriodMax",
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("period and periodMax", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          period: 1,
+          periodMax: 3,
+          periodUnit: "d"
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("every 1 to 3 days");
+  });
+
+  test("periodMax only", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          periodMax: 3,
+          periodUnit: "d"
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("every 3 days");
+  });
+
+  test("period only", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          period: 1,
+          periodUnit: "d"
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("every day");
+  });
+});
