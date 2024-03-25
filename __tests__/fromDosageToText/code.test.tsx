22c1bfe69a25751413a70e02923a03dd0669a872
jy95 <jacques.yakoub@gmail.com>
2024-03-25 23:23:51 +0100

    test: code

---
+++ __tests__/fromDosageToText/code.test.tsx
@@ -0,0 +1,40 @@
+// For typings autocomplete whatever your IDE
+import { expect, test, describe } from "@jest/globals";
+import FhirDosageUtils from "../../src/index";
+
+// types
+import type { Dosage } from "fhir/r4";
+
+describe("fromDosageToText - code", () => {
+  let dosageUtils: FhirDosageUtils;
+
+  beforeAll(async () => {
+    dosageUtils = await FhirDosageUtils.build({
+      displayOrder: ["code"],
+    });
+  });
+
+  test("No code", () => {
+    const dosage: Dosage = {
+      text: "no code",
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("Simple case", () => {
+    const dosage: Dosage = {
+      timing: {
+        code: {
+          text: "Take medication in the morning on weekends and days off work",
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe(
+      "Take medication in the morning on weekends and days off work",
+    );
+  });
+});
