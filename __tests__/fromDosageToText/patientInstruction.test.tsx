56d301267ff6497fa90a17c8351907767f686671
jy95 <jacques.yakoub@gmail.com>
2024-03-25 22:40:03 +0100

    test: timeOfDay / patientInstruction

---
+++ __tests__/fromDosageToText/patientInstruction.test.tsx
@@ -0,0 +1,34 @@
+// For typings autocomplete whatever your IDE
+import { expect, test, describe } from "@jest/globals";
+import FhirDosageUtils from "../../src/index";
+
+// types
+import type { Dosage } from "fhir/r4";
+
+describe("fromDosageToText - patientInstruction", () => {
+  let dosageUtils: FhirDosageUtils;
+
+  beforeAll(async () => {
+    dosageUtils = await FhirDosageUtils.build({
+      displayOrder: ["patientInstruction"],
+    });
+  });
+
+  test("No patientInstruction", () => {
+    const dosage: Dosage = {
+      text: "no patientInstruction",
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("Simple case", () => {
+    const dosage: Dosage = {
+      patientInstruction: "patientInstruction",
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("patientInstruction");
+  });
+});
---
+++ __tests__/fromDosageToText/timeOfDay.test.tsx
@@ -0,0 +1,64 @@
+// For typings autocomplete whatever your IDE
+import { expect, test, describe } from "@jest/globals";
+import FhirDosageUtils from "../../src/index";
+
+// types
+import type { Dosage } from "fhir/r4";
+
+describe("fromDosageToText - timeOfDay", () => {
+  let dosageUtils: FhirDosageUtils;
+
+  beforeAll(async () => {
+    dosageUtils = await FhirDosageUtils.build({
+      displayOrder: ["timeOfDay"],
+    });
+  });
+
+  test("No timeOfDay", () => {
+    const dosage: Dosage = {
+      text: "no timeOfDay",
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("timeOfDay - empty", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          timeOfDay: [],
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("timeOfDay - 1 item", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          timeOfDay: ["15:00:00"],
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("at 15:00");
+  });
+
+  test("timeOfDay - N+1 items", () => {
+    const dosage: Dosage = {
+      timing: {
+        repeat: {
+          timeOfDay: ["15:00:00", "12:12:12"],
+        },
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("at 15:00 and 12:12:12");
+  });
+});
