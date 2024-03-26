dc7d95e01869a02267816a3a60046b0fa418b538
jy95 <jacques.yakoub@gmail.com>
2024-03-26 20:18:09 +0100

    test: corner cases

---
+++ __tests__/corner_cases.test.tsx
@@ -0,0 +1,58 @@
+// For typings autocomplete whatever your IDE
+import { expect, test, describe } from "@jest/globals";
+import FhirDosageUtils from "../src/index";
+
+// types
+import type { Dosage } from "fhir/r4";
+
+describe("Corner cases", () => {
+  let dosageUtils: FhirDosageUtils;
+
+  beforeAll(async () => {
+    dosageUtils = await FhirDosageUtils.build();
+  });
+
+  test("fromCodeableConceptToString - empty coding", () => {
+    const dosage: Dosage = {
+      method: {
+        coding: [],
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("");
+  });
+
+  test("fromCodeableConceptToString - only a code without display", () => {
+    const dosage: Dosage = {
+      method: {
+        coding: [
+          {
+            system: "http://snomed.info/sct",
+            code: "311501008",
+          },
+        ],
+      },
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("311501008");
+  });
+
+  test("fromFHIRQuantityUnitToString - only a code", () => {
+    const dosage: Dosage = {
+      doseAndRate: [
+        {
+          doseQuantity: {
+            value: 42,
+            code: "ml",
+            system: "http://unitsofmeasure.org",
+          },
+        },
+      ],
+    };
+
+    let result = dosageUtils.fromDosageToText(dosage);
+    expect(result).toBe("42 ml");
+  });
+});
