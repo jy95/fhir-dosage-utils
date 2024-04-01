f4360f6c6d4c5819198ae99760e4779fbc3b2838
jy95 <jacques.yakoub@gmail.com>
2024-04-01 13:19:48 +0200

    feat: fromMultipleDosageToText

--- src/utils/fromListToString.ts
+++ src/utils/fromListToString.ts
@@ -1,13 +1,27 @@
 import type { I18N } from "../types";
 
+type LinkwordChoice = "and" | "then";
+type TechnicalKey = "linkwords.and" | "linkwords.then";
+
+const linkworsdMap = {
+  and: "linkwords.and",
+  then: "linkwords.then",
+} satisfies Record<LinkwordChoice, TechnicalKey>;
+
 // To make a list understandable for end-users
-export function fromListToString(i18next: I18N, arr: string[]): string {
+export function fromListToString(
+  i18next: I18N,
+  arr: string[],
+  linkWord: LinkwordChoice = "and",
+): string {
   // Split the array into two parts: all entries except the last one, and the last
   const firstString = arr.slice(0, -1).join(", ");
   const lastString = arr.slice(-1).join("");
 
   // concatenate the result
-  const linkWord = arr.length > 1 ? ` ${i18next.t("linkwords.and")} ` : "";
-  const finalString = firstString + linkWord + lastString;
+  const linkWordTechnicalKey = linkworsdMap[linkWord];
+  const linkWordAsString =
+    arr.length > 1 ? ` ${i18next.t(linkWordTechnicalKey)} ` : "";
+  const finalString = firstString + linkWordAsString + lastString;
   return finalString;
 }
