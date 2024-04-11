0f63097bf63ea2cb2afc52a5e80aa6dc32d1a31a
jy95 <jacques.yakoub@gmail.com>
2024-04-11 08:31:59 +0200

    docs: set up playground (WIP)

--- documentation/playground_types.ts
+++ documentation/playground_types.ts
@@ -4,15 +4,6 @@ import type { Dosage as DosageR5 } from "fhir/r5";
 
 import type { Params } from "fhir-dosage-utils";
 
-// Let's use R4 for Playground from now ...
-// ts-json-schema-generator can't do at the moment union with
-export type Entry = DosageR4; //| DosageR5;
-
-/**
- * @id Input
- */
-export type PlaygroundInput = Entry[];
-
 // No need to enable all extensions
 // https://github.com/vega/ts-json-schema-generator/issues/568
 type DeepExclude<T, A> = T extends A
@@ -20,6 +11,28 @@ type DeepExclude<T, A> = T extends A
   : T extends Record<string, never> | Array<any>
     ? { [K in keyof T]: DeepExclude<T[K], A> }
     : T;
+
+type FilterNotStartingWith<
+  T,
+  Needle extends string,
+> = T extends `${Needle}${infer Rest}` ? never : T;
+
+type DeepOmit<T, K extends string> = T extends object
+  ? {
+      [P in keyof T as FilterNotStartingWith<P, K> extends never
+        ? never
+        : P]: DeepOmit<T[P], K>;
+    }
+  : T;
+
+// Let's use R4 for Playground from now ...
+// ts-json-schema-generator can't do at the moment union with DosageR5
+type Entry = DeepOmit<DosageR4, "_">;
+
+/**
+ * @id Input
+ */
+export type PlaygroundInput = Entry[];
 
 type LighterConfig = DeepExclude<
   Params,
