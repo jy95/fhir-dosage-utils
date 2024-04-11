70baa7f623f0aff70cd84af75916af3ef6726b7f
jy95 <jacques.yakoub@gmail.com>
2024-04-11 10:08:25 +0200

    docs: set up playground (WIP)

--- documentation/playground_types.ts
+++ documentation/playground_types.ts
@@ -12,22 +12,13 @@ type DeepExclude<T, A> = T extends A
     ? { [K in keyof T]: DeepExclude<T[K], A> }
     : T;
 
-type FilterNotStartingWith<
-  T,
-  Needle extends string,
-> = T extends `${Needle}${infer Rest}` ? never : T;
-
-type DeepOmit<T, K extends string> = T extends object
-  ? {
-      [P in keyof T as FilterNotStartingWith<P, K> extends never
-        ? never
-        : P]: DeepOmit<T[P], K>;
-    }
-  : T;
+type DeepOmit<T, K extends string> = {
+  [P in keyof T as Exclude<P, `${K}${string}`>]: T[P] extends object ? DeepOmit<T[P], K> : T[P];
+};
 
 // Let's use R4 for Playground from now ...
 // ts-json-schema-generator can't do at the moment union with DosageR5
-type Entry = DeepOmit<DosageR4, "_">;
+export type Entry = DeepOmit<DosageR4, "_">;
 
 /**
  * @id Input
