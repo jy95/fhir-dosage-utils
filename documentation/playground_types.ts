70b5d7f7d812de09f8ca61da2f827d89f4b3aaeb
jy95 <jacques.yakoub@gmail.com>
2024-04-11 12:00:02 +0200

    docs: set up playground (WIP)

--- documentation/playground_types.ts
+++ documentation/playground_types.ts
@@ -4,14 +4,6 @@ import type { Dosage as DosageR5 } from "fhir/r5";
 
 import type { Params } from "fhir-dosage-utils";
 
-// No need to enable all extensions
-// https://github.com/vega/ts-json-schema-generator/issues/568
-type DeepExclude<T, A> = T extends A
-  ? never
-  : T extends Record<string, never> | Array<any>
-    ? { [K in keyof T]: DeepExclude<T[K], A> }
-    : T;
-
 // Let's use R4 for Playground from now ...
 // ts-json-schema-generator can't do at the moment union with DosageR5
 export type Entry = DosageR4;
@@ -21,7 +13,7 @@ export type Entry = DosageR4;
  */
 export type PlaygroundInput = Entry[];
 
-type LighterConfig = DeepExclude<
+type LighterConfig = Omit<
   Params,
   | "fromFHIRQuantityUnitToString"
   | "fromCodeableConceptToString"
